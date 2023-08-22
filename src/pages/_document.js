d34288efefe2e6afeb4e2fba3a31a7fd91731b34
Aditya Agarwal <adityag2511@gmail.com>
2023-08-22 10:17:34 -0700

    Added a basic NavBar component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import NavBar from '../components/Navbar';
 
 export default function Document() {
   return (
     <Html lang="en" className="h-full bg-gray-50">
       <Head />
       <body>
+        <NavBar />
         <Main />
         <NextScript />
       </body>
